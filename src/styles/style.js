43bec653ca4e254a4bb6906e57dffc4a01c13271
Joao Pedro <jpmuniz88@gmail.com>
2025-11-24 19:06:01 -0300

    Implements style-components global

---
+++ src/styles/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  padding: 30px;
+  max-width: 640px;
+  margin: 0 auto;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
